351b48f9eaa6f91d2891508ade7352371cfb63ad
Nesho99 <gecineven@gmail.com>
2023-06-19 04:59:03 +0200

    Dodan api za blokiranje i deblokiranje

---
+++ api/korisnici/blokiraj.php
@@ -0,0 +1,39 @@
+<?php
+require_once("../../pomocne/Sesija.class.php");
+require_once("../../pomocne/baza.class.php");
+require_once("../../pomocne/QueryBuilder.class.php");
+
+//Provjeri da je admin
+$sesija = new Sesija();
+
+$korisnik = $sesija->dajKorisnika();
+
+
+if ($korisnik == null or $korisnik["uloga"] != "admin") {
+    $rez = new stdClass;
+    $rez->uspijeh = false;
+    $rez->poruka="Nedovoljno privilegija";
+    die(json_encode($rez));
+}
+if(!isset($_GET["id"])){
+    $rez = new stdClass;
+    $rez->uspijeh = false;
+    $rez->poruka="ID mora biti postavljen";
+    die(json_encode($rez));
+
+}
+
+$id=$_GET["id"];
+
+$baza= new Baza();
+$baza->spojiDB();
+$qb= new QueryBuilder();
+$upit=$qb->update('korisnik')->set(['neuspjesnePrijave'=> 3])->where('id='.$id)->getQuery();
+$baza->updateDB($upit);
+
+$baza->zatvoriDB();
+
+$rez = new  stdClass;
+$rez->uspijeh=true;
+echo json_encode($rez);
+?>

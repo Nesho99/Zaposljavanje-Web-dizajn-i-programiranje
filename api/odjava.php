289deb103c82a86181a72137fa1c7897d2ca0d50
Nesho99 <gecineven@gmail.com>
2023-06-23 20:18:47 +0200

    prijava i odjava loging

--- api/odjava.php
+++ api/odjava.php
@@ -1,9 +1,36 @@
 <?php
 require_once("../pomocne/Sesija.class.php");
+require_once("../pomocne/baza.class.php");
+require_once("../modeli/Korisnik.class.php");
+require_once("../modeli/Dnevnik.class.php");
+require_once("../pomocne/datumi.lib.php");
+require_once("../pomocne/VirtualnoVrijeme.class.php");
+require_once("../pomocne/QueryBuilder.class.php");
+
+$dnevnik = new Dnevnik();
+
+$dnevnik->tip = "odjava";
+print_r(Sesija::dajKorisnika());
+$dnevnik->korisnik = Sesija::dajKorisnika()["korisnik"];
 
 // Set the expiration date to one hour ago
 setcookie("prijava_sesija", "", time() - 3600, "/");
 unset($_COOKIE['prijava_sesija']);
+$dnevnik->datumVrijeme = pretvoriUsqlTimestamp(VirtualnoVrijeme::virtualnoSada());
+$dnevnik->tip = "OdjaVA";
+$qb = new QueryBuilder();
+
+$baza = new Baza();
+$baza->spojiDB();
+$koriIme= $dnevnik->korisnik = Sesija::dajKorisnika()["korisnik"];
+$upit=$qb->select("id")->from("korisnik")->where("korisnickoIme='".$koriIme."'")->getQuery();
+$rezultat = $baza->selectDB($upit);
+print_r($upit);
+
+$dnevnik->korisnik = $rezultat->fetch_assoc()["id"];
+$upit = $qb->insertInto("dnevnik", ["korisnik", "tip", "datumVrijeme"])->values([ $dnevnik->korisnik, $dnevnik->tip, $dnevnik->datumVrijeme])->getQuery();
+$baza->updateDB($upit);
+$baza->zatvoriDB();
 header("Location: ../index.php");
 
 
--- api/prijava.php
+++ api/prijava.php
@@ -2,6 +2,14 @@
 require_once("../pomocne/baza.class.php");
 require_once("../modeli/Korisnik.class.php");
 require_once("../pomocne/Sesija.class.php");
+require_once("../modeli/Dnevnik.class.php");
+require_once("../pomocne/datumi.lib.php");
+require_once("../pomocne/VirtualnoVrijeme.class.php");
+require_once("../pomocne/QueryBuilder.class.php");
+
+
+
+
 if (empty($_POST["kime"])) {
     die("Unesite korisnicko ime");
 }
@@ -49,17 +57,36 @@ if (isset($_POST["zapamti"])) {
     $vrijemeIsteka = time() + (24 * 60 * 60); // 24 sata * 60 minuta * 60 sekundi
 
     // Postavi kolačić s željenim vremenom isteka
-    setcookie($imeKolacica, $vrijednostKolacica, $vrijemeIsteka,"/");
+    setcookie($imeKolacica, $vrijednostKolacica, $vrijemeIsteka, "/");
+
 
     echo "Zapamcen";
-
-   
-
+    
 
 }
+$dnevnik = new Dnevnik();
+$baza = new Baza();
+$qb = new QueryBuilder();
+$baza->spojiDB();
+
+$upit = $qb->select("id")->from("korisnik")->where("korisnickoIme='" . $korisnickoIme . "'")->getQuery();
+$rezultat = $baza->selectDB($upit);
+
+$redak = $rezultat->fetch_assoc();
+print_r("Rezultat: " . $redak);
+
+$dnevnik->korisnik = $redak["id"];
+print_r($dnevnik);
+$dnevnik->tip="Prijava";
+$dnevnik->datumVrijeme= pretvoriUsqlTimestamp(VirtualnoVrijeme::virtualnoSada());
+$upit = $qb->insertInto("dnevnik", ["korisnik", "tip", "datumVrijeme"])
+    ->values([$dnevnik->korisnik, $dnevnik->tip, $dnevnik->datumVrijeme])->getQuery();
+
+$baza->updateDB($upit);
+print_r("Upit 2: " . $upit);
+$baza->updateDB($upit);
 header("Location: ../index.php");
 $baza->zatvoriDB();
-
 
 
 
